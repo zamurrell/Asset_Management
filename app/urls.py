8fe03f0edfd516ba701214a99061ebc51e6d7e5a
Zach Murrell <zamurrell@gmail.com>
2020-12-10 18:47:12 -0600

    Moved app routing - urls.py into app from project

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('welcome', views.welcome),
+    path('types', views.types),
+]
--- asset_management/urls.py
+++ asset_management/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('welcome', app.views.welcome),
-    path('types', app.views.types),
+    path('app/', include('app.urls')),
 ]
