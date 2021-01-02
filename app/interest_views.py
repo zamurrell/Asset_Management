13b856f3990589690a150a37cbc0389559cc5611
Zach Murrell <zamurrell@gmail.com>
2021-01-02 17:11:32 -0600

    Persist input values on submit

--- app/interest_views.py
+++ app/interest_views.py
@@ -6,6 +6,6 @@ def interest(request):
         amount = float(request.GET['amount'])
         rate = float(request.GET['rate'])
         interest = amount * rate / 100
-        return render(request, 'interest.html', {'interest': interest})
+        return render(request, 'interest.html', {'interest': interest, 'amount': amount, 'rate': rate})
     else:
         return render(request, 'interest.html')
