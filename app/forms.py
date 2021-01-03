a1a02ec4883d6a586967b06c462a5711c619e952
Zach Murrell <zamurrell@gmail.com>
2021-01-03 10:05:24 -0600

    Two distinct forms, defined with html and with Python class

---
+++ app/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class InterestForm(forms.Form):
+    amount = forms.FloatField(label='Amount')
+    rate = forms.FloatField(label='Interest Rate', min_value=5, max_value=50)
+
+
+class AuthorForm(forms.Form):
+    fullname = forms.CharField(label="Fullname")
+    email = forms.EmailField(label="Email")
+    mobile = forms.RegexField(label="Mobile", regex=r"\d+")
--- app/urls.py
+++ app/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('interest_post/', interest_views.interest_post),
     # Author URLS
     path('authors/', author_views.list_authors),
+    path('add_author/', author_views.add_author),
+    path('add_author_form/', author_views.add_author_form),
 ]
