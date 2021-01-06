cb98d6646e289b34e2fc4faaeb5e05d2db789fd4
Zach Murrell <zamurrell@gmail.com>
2021-01-05 20:08:34 -0600

    Recreate interest rate calc form with Django form

--- app/interest_views.py
+++ app/interest_views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import InterestForm
 
 
 def interest(request):
@@ -19,3 +20,35 @@ def interest_post(request):
         return render(request, 'interest_post.html', {'interest': interest, 'amount': amount, 'rate': rate})
     else:
         return render(request, 'interest_post.html')
+
+
+def interest_form(request):
+    if request.method == "POST":
+        form = InterestForm(request.POST)  # bind POST data to form
+        if form.is_valid():
+            amount = float(form.cleaned_data['amount'])
+            rate = float(form.cleaned_data['rate'])
+            interest = amount * rate / 100
+            return render(request, 'interest_form.html',
+                          {'form': form, 'interest': interest})
+        else:
+            return render(request, 'interest_form.html', {'form': form})
+    else:  # GET request
+        form = InterestForm()
+        return render(request, 'interest_form.html', {'form': form})
+
+
+def interest_form_custom(request):
+    if request.method == "POST":
+        form = InterestForm(request.POST)  # bind POST data to form
+        if form.is_valid():
+            amount = float(form.cleaned_data['amount'])
+            rate = float(form.cleaned_data['rate'])
+            interest = amount * rate / 100
+            return render(request, 'interest_form_custom.html',
+                          {'form': form, 'interest': interest})
+        else:
+            return render(request, 'interest_form_custom.html', {'form': form})
+    else:  # GET request
+        form = InterestForm()
+        return render(request, 'interest_form_custom.html', {'form': form})
--- app/urls.py
+++ app/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('authors/', author_views.list_authors),
     path('add_author/', author_views.add_author),
     path('add_author_form/', author_views.add_author_form),
+    # Form URLs
+    path('interest_form/', interest_views.interest_form),  # Using Django Form
+    path('interest_form_custom/', interest_views.interest_form_custom),
 ]
